902ba2da851fb711dc03ab7244c6a363c403f01c
matiasrgomez <matiasrgomez@hotmail.com>
2023-05-18 09:47:43 -0300

    correcciones y prueba de validar

--- validar.js
+++ validar.js
@@ -1,1 +1,18 @@
--
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("formulario").addEventListener('submit', validarFormulario); 
+  });
+  
+  function validarFormulario(evento) {
+    evento.preventDefault();
+    var Name = document.getElementById('F-Name').value;
+    if(usuario.length == 0) {
+      alert('No has escrito nada en el usuario');
+      return;
+    }
+    var Apellido = document.getElementById('F-Apellido').value;
+    if (clave.length < 6) {
+      alert('La clave no es válida');
+      return;
+    }
+    this.submit();
+  }
