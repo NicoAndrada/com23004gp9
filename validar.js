9bda8b546caaeb7e66649605bc6cf2eea3c4757b
matiasrgomez <matiasrgomez@hotmail.com>
2023-05-21 23:39:15 -0300

    Ya funciona validacion

--- validar.js
+++ validar.js
@@ -1,18 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("formulario").addEventListener('submit', validarFormulario); 
-  });
-  
-  function validarFormulario(evento) {
-    evento.preventDefault();
-    var Name = document.getElementById('F-Name').value;
-    if(usuario.length == 0) {
-      alert('No has escrito nada en el usuario');
-      return;
-    }
-    var Apellido = document.getElementById('F-Apellido').value;
-    if (clave.length < 6) {
-      alert('La clave no es válida');
-      return;
-    }
-    this.submit();
+const nombre = document.getElementById("Name")
+const apellido = document.getElementById("Apellido")
+const telefono = document.getElementById("Tel")
+const email = document.getElementById("Email")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("Warnings")
+
+form.addEventListener("submit", e=>{
+  e.preventDefault()
+  let warnings = ""
+  let entrar = false
+  let ValidaEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+  parrafo.innerHTML = ""
+  if(nombre.value.length <6){
+      warnings += `El nombre no es valido <br>`
+      entrar = true
   }
+  if(apellido.value.length < 1){
+    warnings += `Ingrese el apellido <br>`
+    entrar = true
+}
+  if(!ValidaEmail.test(email.value)){
+      warnings += `El email no es valido <br>`
+      entrar = true
+  }
+  
+  if (telefono.value.length < 8){
+    warnings += 'El telefono no es valido <br>'
+  }
+
+  if(entrar){
+      parrafo.innerHTML = warnings
+  }else{
+      parrafo.innerHTML = "Enviado"
+  }
+})
+
